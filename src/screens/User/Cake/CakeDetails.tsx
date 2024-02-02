126c0c87c3dd92a97150a51ce02de45ebb8bd950
Artan Hysenaj <artan.hysenaj@gmail.com>
2024-02-02 23:19:33 +0100

    Refactor cake details and cakes list components

--- src/screens/User/Cake/CakeDetails.tsx
+++ src/screens/User/Cake/CakeDetails.tsx
@@ -1,8 +1,35 @@
-import { Fragment } from "react";
 import { useParams } from "react-router-dom";
+
+const DUMMY_COMMENTS = [
+  {
+    user: "john doe",
+    timestamp: "2021-01-01",
+    comment: "This is a comment",
+  },
+];
 
 export const Component = function CakeDetails(): JSX.Element {
   const { id } = useParams();
 
-  return <Fragment>CakeDetails: {id}</Fragment>;
+  return (
+    <div className="flex w-full">
+      <div className="w-full">
+        <div>
+          <img src="https://via.placeholder.com/500" alt="" />
+        </div>
+        <div>
+          <h5>Comments</h5>
+          {DUMMY_COMMENTS.map((comment) => (
+            <div>
+              <p className="text-slate-600">
+                <span>{comment.user}</span> - <span>{comment.timestamp}</span>
+              </p>
+              <p>{comment.comment}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-full">details: {id}</div>
+    </div>
+  );
 };
--- src/screens/User/Cake/CakesList.tsx
+++ src/screens/User/Cake/CakesList.tsx
@@ -1,5 +1,104 @@
-import { Fragment } from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Link } from "react-router-dom";
+
+const DUMMY_CAKES = [
+  {
+    id: 1,
+    image: "https://via.placeholder.com/150",
+    title: "Cake 1",
+    description: "Cake 1 description",
+  },
+  {
+    id: 2,
+    image: "https://via.placeholder.com/150",
+    title: "Cake 2",
+    description: "Cake 2 description",
+  },
+  {
+    id: 3,
+    image: "https://via.placeholder.com/150",
+    title: "Cake 3",
+    description: "Cake 3 description",
+  },
+];
 
 export const Component = function CakesList(): JSX.Element {
-  return <Fragment>CakesList</Fragment>;
+  return (
+    <div className="space-y-14 mb-20">
+      <div>
+        <h3 className="text-3xl font-bold">Promotions</h3>
+        <br />
+        <ul className="flex gap-4">
+          {DUMMY_CAKES.map((cake) => (
+            <li className="w-full">
+              <Card>
+                <CardHeader>
+                  <div className="rounded overflow-hidden">
+                    <Link to={`${cake.id}`}>
+                      <img
+                        className="block mx-auto"
+                        width={150}
+                        height={150}
+                        src={cake.image}
+                        alt={cake.description}
+                      />
+                    </Link>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <Link to={`${cake.id}`}>
+                    <CardTitle>{cake.title}</CardTitle>
+                  </Link>
+                  <CardDescription>{cake.description}</CardDescription>
+                </CardContent>
+                <CardFooter>
+                  <p>Card Footer</p>
+                </CardFooter>
+              </Card>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h3 className="text-3xl font-bold">Menu</h3>
+        <br />
+        <ul className="flex flex-col gap-4">
+          {DUMMY_CAKES.map((cake) => (
+            <li className="w-full">
+              <Card>
+                <CardHeader>
+                  <div className="rounded overflow-hidden">
+                    <Link to={`${cake.id}`}>
+                      <img
+                        width={150}
+                        height={150}
+                        src={cake.image}
+                        alt={cake.description}
+                      />
+                    </Link>
+                  </div>
+                </CardHeader>
+                <CardContent>
+                  <Link to={`${cake.id}`}>
+                    <CardTitle>{cake.title}</CardTitle>
+                  </Link>
+                  <CardDescription>{cake.description}</CardDescription>
+                </CardContent>
+                <CardFooter>
+                  <p>Card Footer</p>
+                </CardFooter>
+              </Card>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
